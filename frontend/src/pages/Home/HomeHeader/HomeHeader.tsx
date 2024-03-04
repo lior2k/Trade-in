c814c6e9ea276933cd4013b871effaed4367fb5b
Lior <Lior2kx@gmail.com>
2024-03-04 02:19:49 +0200

    home page header section bottom form bottoms - non functional for now but hey, atleast they look good

--- frontend/src/pages/Home/HomeHeader/HomeHeader.tsx
+++ frontend/src/pages/Home/HomeHeader/HomeHeader.tsx
@@ -3,6 +3,7 @@ import NavBar from '../../../components/NavBar/NavBar';
 import SearchBar from '../../../components/SearchBar/SearchBar';
 import { useNavigate } from 'react-router-dom';
 import { CarData } from '../../../constants/constants';
+import { Icon } from '@iconify/react';
 
 const HomeHeader = () => {
 	const navigate = useNavigate();
@@ -18,8 +19,36 @@ const HomeHeader = () => {
 				<span className='sub-text'>מצא רכבים למכירה ולהחלפה</span>
 				<SearchBar onSearch={navigateToSearchPage} />
 			</div>
+
+			<div className='header-bottom-form-wrapper'>
+				<span
+					className='sub-text'
+					style={{ display: 'block', marginBlock: '.75rem' }}
+				>
+					או חפש מודל ספציפי
+				</span>
+				<div className='header-bottom-form'>
+					<button className='transparent-button'>
+						<span>חשמלי</span>
+						<Icon icon='mdi:electric-charger'></Icon>
+					</button>
+					<button className='transparent-button'>
+						<span>גי'פ</span>
+						<Icon icon='mingcute:jeep-line'></Icon>
+					</button>
+					<button className='transparent-button'>
+						<span>מסחרי</span>
+						<Icon icon='mdi:truck-outline'></Icon>
+					</button>
+				</div>
+			</div>
 		</div>
 	);
+	// mingcute:jeep-line גי'פ
+	// mdi:electric-charger חשמלי
+	// mdi:truck-outline מסחרי
+	// mdi:car-sports ספורט
+	//
 };
 
 export default HomeHeader;
--- frontend/src/pages/Home/HomePageCarousel/HomePageCarousel.tsx
+++ frontend/src/pages/Home/HomePageCarousel/HomePageCarousel.tsx
@@ -29,39 +29,38 @@ const HomePageCarousel: React.FC<{ carList: CarData[] }> = ({ carList }) => {
 
 	return (
 		<div className='carousel-outer-wrapper bglightgrey top-minus'>
-<div className='carousel-wrapper'>
-			<div className='title-wrapper'>
-				<h2 className='section-title'>רכבים מובילים</h2>
-				<span className='sub-text theme-link-text'>
-					לכל הרכבים<Icon icon='uit:arrow-up-left'></Icon>
-				</span>
+			<div className='carousel-wrapper'>
+				<div className='title-wrapper'>
+					<h2 className='section-title'>רכבים מובילים</h2>
+					<span className='sub-text theme-link-text'>
+						לכל הרכבים<Icon icon='uit:arrow-up-left'></Icon>
+					</span>
+				</div>
+				<Carousel
+					swipeable={false}
+					draggable={false}
+					// showDots={true}
+					responsive={responsive}
+					ssr={false} // means to render carousel on server-side.
+					infinite={true}
+					//   autoPlay={this.props.deviceType !== "mobile" ? true : false}
+					autoPlay={true}
+					autoPlaySpeed={3000}
+					keyBoardControl={true}
+					customTransition='all .5s ease-in-out'
+					// transitionDuration={500}
+					containerClass='carousel-container'
+					// removeArrowOnDeviceType={['tablet', 'mobile']}
+					//   deviceType={this.props.deviceType}
+					// dotListClass='custom-dot-list-style'
+					itemClass='carousel-item'
+				>
+					{carList.map((carData, index) => (
+						<ListItem carData={carData} key={index} onPress={() => {}} />
+					))}
+				</Carousel>
 			</div>
-			<Carousel
-				swipeable={false}
-				draggable={false}
-				// showDots={true}
-				responsive={responsive}
-				ssr={false} // means to render carousel on server-side.
-				infinite={true}
-				//   autoPlay={this.props.deviceType !== "mobile" ? true : false}
-				autoPlay={true}
-				autoPlaySpeed={3000}
-				keyBoardControl={true}
-				customTransition='all .5s ease-in-out'
-				// transitionDuration={500}
-				containerClass='carousel-container'
-				// removeArrowOnDeviceType={['tablet', 'mobile']}
-				//   deviceType={this.props.deviceType}
-				// dotListClass='custom-dot-list-style'
-				itemClass='carousel-item'
-			>
-				{carList.map((carData, index) => (
-					<ListItem carData={carData} key={index} onPress={() => {}} />
-				))}
-			</Carousel>
 		</div>
-		</div>
-		
 	);
 };
 
