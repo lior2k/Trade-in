eda74996dd506d8b84ebad6ae329e68123a6e2dd
Lior <Lior2kx@gmail.com>
2024-03-05 23:05:51 +0200

    widgets done

--- frontend/src/components/Widget/Widget.tsx
+++ frontend/src/components/Widget/Widget.tsx
@@ -1,21 +1,31 @@
-import React from "react";
-import "./Widget.css";
-import { Icon } from "@iconify/react";
+import React from 'react';
+import './Widget.css';
+import { Icon } from '@iconify/react';
 
 interface WidgetProps {
-  icon: string;
-  title: string;
-  description: string;
+	icon: string;
+	title: string;
+	description: string;
+	addColor?: boolean;
 }
 
-const Widget: React.FC<WidgetProps> = ({ icon, title, description }) => {
-  return (
-    <div className="widget-container bradius">
-      <Icon className="big-icon" icon={icon}></Icon>
-      <span className="mini-title">{title}</span>
-      <span className="sub-text">{description}</span>
-    </div>
-  );
+const Widget: React.FC<WidgetProps> = ({
+	icon,
+	title,
+	description,
+	addColor,
+}) => {
+	return (
+		<div className='widget-container bradius'>
+			<Icon
+				className={`big-icon`}
+				icon={icon}
+				style={addColor ? { color: 'var(--theme-color)' } : {}}
+			></Icon>
+			<span className='mini-title'>{title}</span>
+			<span className='sub-text'>{description}</span>
+		</div>
+	);
 };
 
 export default Widget;
--- frontend/src/components/WidgetBox/WidgetBox.tsx
+++ frontend/src/components/WidgetBox/WidgetBox.tsx
@@ -1,35 +1,36 @@
-import React from "react";
-import "./WidgetBox.css";
-import Widget from "../Widget/Widget";
+import React from 'react';
+import './WidgetBox.css';
+import Widget from '../Widget/Widget';
 
 const WidgetBox = () => {
-  return (
-    <div className="widget-box-container">
-      <h2 className="section-title">למה לבחור בנו?</h2>
-      <div className="widget-box">
-        <Widget
-          icon="noto:bank"
-          title="אפשרויות מימון מרובות"
-          description="בטרייד-אין-הרצליה אנחנו מחוייבים להפוך את החלומות שלכם למציאות, אנחנו מספקים עד 100% מימון בתנאים הכי טובים שיש"
-        />
-        <Widget
-          icon="noto:bank"
-          title="אפשרויות מימון מרובות"
-          description="בטרייד-אין-הרצליה אנחנו מחוייבים להפוך את החלומות שלכם למציאות, אנחנו מספקים עד 100% מימון בתנאים הכי טובים שיש"
-        />
-        <Widget
-          icon="noto:bank"
-          title="אפשרויות מימון מרובות"
-          description="בטרייד-אין-הרצליה אנחנו מחוייבים להפוך את החלומות שלכם למציאות, אנחנו מספקים עד 100% מימון בתנאים הכי טובים שיש"
-        />
-        <Widget
-          icon="noto:bank"
-          title="אפשרויות מימון מרובות"
-          description="בטרייד-אין-הרצליה אנחנו מחוייבים להפוך את החלומות שלכם למציאות, אנחנו מספקים עד 100% מימון בתנאים הכי טובים שיש"
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className='widget-box-container'>
+			<h2 className='section-title'>למה לבחור בנו?</h2>
+			<div className='widget-box'>
+				<Widget
+					icon='noto:bank'
+					title='אפשרויות מימון'
+					description='בטרייד-אין-הרצליה אנחנו מחוייבים להפוך את החלומות שלכם למציאות, אנחנו מספקים עד 100% מימון בתנאים הכי טובים שיש'
+				/>
+				<Widget
+					icon='noto:shield'
+					title='אמינות'
+					description='מאז שהוקמה הסוכנות אי שם בשנות ה90 בהנהגת אברהם שחוח, התחייבנו לתת את השירות הטוב ביותר והאמין ביותר'
+				/>
+				<Widget
+					icon='noto:euro-banknote'
+					title='נוח לכל כיס'
+					description='אצלנו בטרייד אין הרצליה, תמצאו את המחירים השווים ביותר בשוק, כי כשמקצועיות פוגשת יושרה, כולם מרווחים'
+				/>
+				<Widget
+					icon='mdi:car-repair'
+					title='מקצועיות'
+					description='חוויה מקצועית עם שירות רכב מקיף. נתחייב לדיוק ושביעות רצונך, ונבטיח שכל ביקור במוסך שלנו הוא מסע של שירות גבוה ושמירה מקצועית על כל רכב.'
+					addColor={true}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default WidgetBox;
